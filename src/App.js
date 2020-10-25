a89eac3d3e2d4ed0ba74f3ce6e5b6a629007c051
Oliver Ni <oliver.ni@gmail.com>
2020-10-25 00:52:38 -0700

    Finish up features

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+/* eslint jsx-a11y/anchor-is-valid: 0 */
+
 import useComponentSize from "@rehooks/component-size";
 import "bulma/css/bulma.min.css";
+import "katex/dist/katex.min.css";
 import React, { useRef } from "react";
-import { Circle, Layer, Line, Stage } from "react-konva";
+import { InlineMath } from "react-katex";
+import { Circle, Layer, Line, Stage, Text } from "react-konva";
 import { animated, interpolate, useSpring, useSprings } from "react-spring";
 import "./App.css";
 import { usePoints } from "./hooks";
 
 const AnimatedCircle = animated(Circle);
 const AnimatedLine = animated(Line);
+const AnimatedText = animated(Text);
 
 const Colors = {
     POINT: "#e13f3f",
@@ -16,9 +21,19 @@ const Colors = {
     RESID: "#757575",
 };
 
+const textProps = {
+    fontFamily:
+        'BlinkMacSystemFont,-apple-system,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Fira Sans","Droid Sans","Helvetica Neue",Helvetica,Arial,sans-serif',
+    fontSize: 16,
+    padding: 16,
+};
+
+const negate = (x) => -x;
+const round = (x) => x.toFixed(2);
+
 const Canvas = ({
     points,
-    addPoint,
+    addPoint: _addPoint,
     removePoint,
     allowAdd,
     stats,
@@ -32,12 +47,11 @@ const Canvas = ({
         points.map((p) => ({ x: p[0], y: p[1] }))
     );
 
-    const { mx, my, b1, b0 } = useSpring({
-        mx: stats[0],
-        my: stats[1],
-        b1: stats[2],
-        b0: stats[3],
-    });
+    const { mx, my, b1, b0, r2 } = stats;
+
+    const addPoint = (e) => {
+        _addPoint(e.evt.layerX - w / 2, e.evt.layerY - h / 2);
+    };
 
     return (
         <animated.div className="points-container" ref={ref} {...props}>
@@ -46,61 +60,186 @@ const Canvas = ({
                 height={h}
                 onClick={allowAdd ? addPoint : undefined}
             >
-                <Layer>
-                    <AnimatedLine
-                        points={interpolate([mx], (mx) => [mx, 0, mx, h])}
-                        stroke={Colors.GRID}
-                    />
-                    <AnimatedLine
-                        points={interpolate([my], (my) => [0, my, w, my])}
-                        stroke={Colors.GRID}
-                    />
-
-                    {springs.map(({ x, y }, idx) => (
+                {points.length === 0 ? (
+                    <Layer>
+                        <Text
+                            {...textProps}
+                            width={w}
+                            height={h}
+                            align="center"
+                            verticalAlign="middle"
+                            text={
+                                allowAdd
+                                    ? "Click to add points"
+                                    : "No points to display."
+                            }
+                        />
+                    </Layer>
+                ) : (
+                    <Layer offsetX={-w / 2} offsetY={-h / 2}>
                         <AnimatedLine
-                            points={interpolate(
-                                [x, y, b1, b0],
-                                (x, y, b1, b0) => [x, y, x, b1 * x + b0]
-                            )}
-                            stroke={Colors.RESID}
-                            key={idx}
+                            points={mx.interpolate((mx) => [
+                                mx,
+                                -h / 2,
+                                mx,
+                                h / 2,
+                            ])}
+                            stroke={Colors.GRID}
                         />
-                    ))}
-
-                    <AnimatedLine
-                        points={interpolate([b0, b1], (b0, b1) => [
-                            0,
-                            b0,
-                            w,
-                            b1 * w + b0,
-                        ])}
-                        stroke={Colors.LINE}
-                    />
-
-                    {springs.map(({ x, y }, idx) => (
-                        <AnimatedCircle
-                            x={x}
-                            y={y}
-                            radius={8}
-                            fill={Colors.POINT}
-                            key={idx}
-                            onClick={() => removePoint(idx)}
+                        <AnimatedLine
+                            points={my.interpolate((my) => [
+                                -w / 2,
+                                my,
+                                w / 2,
+                                my,
+                            ])}
+                            stroke={Colors.GRID}
                         />
-                    ))}
-                </Layer>
+
+                        {springs.map(({ x, y }, idx) => (
+                            <AnimatedLine
+                                points={interpolate(
+                                    [x, y, b1, b0],
+                                    (x, y, b1, b0) => [x, y, x, b1 * x + b0]
+                                )}
+                                stroke={Colors.RESID}
+                                key={idx}
+                            />
+                        ))}
+
+                        <AnimatedLine
+                            points={interpolate([b0, b1], (b0, b1) => [
+                                -w / 2,
+                                b1 * (-w / 2) + b0,
+                                w / 2,
+                                b1 * (w / 2) + b0,
+                            ])}
+                            stroke={Colors.LINE}
+                        />
+
+                        {springs.map(({ x, y }, idx) => (
+                            <AnimatedCircle
+                                x={x}
+                                y={y}
+                                radius={8}
+                                fill={Colors.POINT}
+                                key={idx}
+                                onClick={() => removePoint(idx)}
+                            />
+                        ))}
+
+                        <AnimatedText
+                            {...textProps}
+                            width={w / 2}
+                            height={h / 2}
+                            align="right"
+                            verticalAlign="bottom"
+                            text={r2.interpolate((r2) => `R² = ${round(r2)}`)}
+                        />
+                    </Layer>
+                )}
             </Stage>
         </animated.div>
+    );
+};
+
+const Histogram = ({ points }) => {
+    let ref = useRef(null);
+    let { height: h } = useComponentSize(ref);
+
+    const numBars = Math.floor(h / 50);
+
+    const springs = useSprings(
+        numBars,
+        points
+            .reduce((acc, [, yr]) => {
+                const y = yr + h / 2;
+                acc[Math.floor(y / 50)] += 1;
+                return acc;
+            }, Array(numBars).fill(0))
+            .map((x) => ({
+                width: x,
+            }))
+    );
+
+    return (
+        <div className="histogram-container" ref={ref}>
+            {springs.map(({ width }, idx) => (
+                <animated.div
+                    key={idx}
+                    style={{ width: width.interpolate((w) => w * 40) }}
+                ></animated.div>
+            ))}
+        </div>
+    );
+};
+
+const StatsTable = ({ stats: { mx, my, sx, sy, r2 } }) => (
+    <table class="table control-table">
+        <tbody>
+            <tr>
+                <th>X Mean</th>
+                <td>
+                    <InlineMath>\bar x</InlineMath>
+                </td>
+                <animated.td>{mx.interpolate(round)}</animated.td>
+            </tr>
+            <tr>
+                <th>Y Mean</th>
+                <td>
+                    <InlineMath>\bar y</InlineMath>
+                </td>
+                <animated.td>
+                    {my.interpolate(negate).interpolate(round)}
+                </animated.td>
+            </tr>
+            <tr>
+                <th>X SD</th>
+                <td>
+                    <InlineMath>\sigma_x</InlineMath>
+                </td>
+                <animated.td>{sx.interpolate(round)}</animated.td>
+            </tr>
+            <tr>
+                <th>Y SD</th>
+                <td>
+                    <InlineMath>\sigma_y</InlineMath>
+                </td>
+                <animated.td>{sy.interpolate(round)}</animated.td>
+            </tr>
+            <tr>
+                <th>
+                    R<sup>2</sup>
+                </th>
+                <td>
+                    <InlineMath>R^2</InlineMath>
+                </td>
+                <animated.td>{r2.interpolate(round)}</animated.td>
+            </tr>
+        </tbody>
+    </table>
+);
+
+const Control = ({ points, stats }) => {
+    return (
+        <div className="control-container">
+            <p className="title">
+                R<sup>2</sup> Visualizer
+            </p>
+            {/* <StatsTable stats={stats} /> */}
+            <Histogram points={points} />
+        </div>
     );
 };
 
 const App = () => {
     const {
         points,
-        stats,
         showResids,
         addPoint,
         removePoint,
         setShowResids,
+        stats: _stats,
     } = usePoints();
 
     const style = useSpring({
@@ -108,10 +247,12 @@ const App = () => {
         cursor: showResids ? "not-allowed" : "initial",
     });
 
+    const stats = useSpring(_stats);
+
     return (
         <div className="all-wrapper">
-            <div className="box points-wrapper">
-                <div class="tabs is-centered mb-0">
+            <div className="box all-container">
+                <div className="tabs is-centered mb-0">
                     <ul>
                         <li className={showResids ? "" : "is-active"}>
                             <a onClick={() => setShowResids(false)}>
@@ -125,14 +266,34 @@ const App = () => {
                         </li>
                     </ul>
                 </div>
-                <Canvas
-                    points={points}
-                    addPoint={addPoint}
-                    removePoint={removePoint}
-                    allowAdd={!showResids}
-                    stats={stats}
-                    style={style}
-                />
+                <div className="columns is-gapless is-mobile">
+                    <div className="column is-3">
+                        <Control points={points} stats={stats} />
+                    </div>
+                    <div className="column">
+                        <Canvas
+                            points={points}
+                            addPoint={addPoint}
+                            removePoint={removePoint}
+                            allowAdd={!showResids}
+                            stats={stats}
+                            style={style}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="section foot container">
+                <div className="content has-text-centered">
+                    <p>
+                        <strong>
+                            R<sup>2</sup> Visualizer
+                        </strong>{" "}
+                        by <a href="https://oliverni.com/">Oliver Ni</a>.{" "}
+                        <a href="https://github.com/oliver-ni/rsquared">
+                            View on GitHub.
+                        </a>
+                    </p>
+                </div>
             </div>
         </div>
     );
--- src/hooks.js
+++ src/hooks.js
@@ -1,42 +1,43 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import regression from "regression";
 
-export const useWindowSize = () => {
-    const [windowSize, setWindowSize] = useState({
-        w: window.innerWidth,
-        h: window.innerHeight,
+const initialStats = { mx: -1, my: -1, sx: 0, sy: 0, b0: 0, b1: 0, r2: 0 };
+
+const mean = (data) => {
+    const sum = data.reduce((acc, val) => acc + val, 0);
+    return sum / data.length;
+};
+
+const stdev = (data) => {
+    const avg = mean(data);
+
+    const squareDiffs = data.map((val) => {
+        var diff = val - avg;
+        var sqrDiff = diff * diff;
+        return sqrDiff;
     });
 
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowSize({
-                w: window.innerWidth,
-                h: window.innerHeight,
-            });
-        };
+    const variance = mean(squareDiffs);
+    const stdDev = Math.sqrt(variance);
 
-        window.addEventListener("resize", handleResize);
-
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
-
-    return windowSize;
+    return stdDev;
 };
 
 const getStats = (points) => {
-    if (points.length === 0) return [-1, -1, 0, -1];
+    if (points.length === 0) return initialStats;
 
     const result = regression.linear(points);
     const b1 = result.equation[0];
     const b0 = result.equation[1];
+    const r2 = isNaN(result.r2) ? 0 : result.r2;
 
-    const [sx, sy] = points.reduce(([sx, sy], [x, y]) => [sx + x, sy + y], [
-        0,
-        0,
-    ]);
-    const [mx, my] = [sx / points.length, sy / points.length];
+    const mx = mean(points.map((p) => p[0]));
+    const my = mean(points.map((p) => p[1]));
 
-    return [mx, my, b1, b0];
+    const sx = stdev(points.map((p) => p[0]));
+    const sy = stdev(points.map((p) => p[1]));
+
+    return { mx, my, sx, sy, b0, b1, r2 };
 };
 
 export const usePoints = () => {
@@ -44,21 +45,18 @@ export const usePoints = () => {
     const [stats, setStats] = useState(getStats(points));
     const [showResids, setShowResids] = useState(false);
 
-    const [, my, b1, b0] = stats;
+    const { b0, b1 } = stats;
 
-    const resid = points.map(([x, y]) => [x, y - b1 * x - b0 + my]);
-    const residStats = getStats(resid);
+    const resids = points.map(([x, y]) => [x, y - b1 * x - b0]);
+    const residStats = getStats(resids);
 
     const setPoints = (newPoints) => {
         _setPoints(newPoints);
         setStats(getStats(newPoints));
     };
 
-    const addPoint = (e) => {
-        if (e.target !== e.target.getStage()) {
-            return;
-        }
-        setPoints([...points, [e.evt.layerX, e.evt.layerY]]);
+    const addPoint = (x, y) => {
+        setPoints([...points, [x, y]]);
     };
 
     const removePoint = (idx) => {
@@ -76,7 +74,7 @@ export const usePoints = () => {
         setShowResids,
         toggleResids,
         showResids,
-        points: showResids ? resid : points,
+        points: showResids ? resids : points,
         stats: showResids ? residStats : stats,
     };
 };
